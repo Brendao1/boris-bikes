29f6d8ac22d74d71caafe6c3b9aa4bc1db41331c
“Brenda” <odonnell.brenda@gmail.com>
2019-02-07 15:30:43 +0000

    Added bike as instance variable to the DockingStation class. And set attr_reader :bike so it can be visible to all methods.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,20 +1,21 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
   def release_bike
     Bike.new
   end
 
 
   def dock(bike)
-    bike
+    @bike = bike
     # we want the dock method to return a bike so that
     # it passes the unit test.
   end
 
 end
 
-# feature test for docking a bike
+# feature test for docking a bike. We run this in irb.
 #
 # ds = DockingStation.new
 # redbike = Bike.new
